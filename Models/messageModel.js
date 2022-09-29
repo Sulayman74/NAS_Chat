4ba11b7f6baf7db7b3c186e43ea9f7782ae59f01
Abdelkrim <kissab@gmail.com>
2022-09-29 11:18:58 +0200

    modify messageModel.js

---
+++ Models/messageModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+
+const UserSchema = new Mongoose.Schema({
+    
+    messageID: {
+        type: Number,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 100,
+        delivered : Boolean,
+        read : Boolean
+      },
+      datetime: {
+        type: Date,
+        default: Date.now(),
+      },
+      users: {
+        type: mongoose.Schema.ObjectId,
+        ref: "useModel",
+        required: true,
+      },
+      sender: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',
+        required: true
+      },
+},
+{ timestamps: true }
+)
